9969f2020d33a8078c987ac0b357a36862d9be32
Chinmay Purav <chinpurav@gmail.com>
2025-07-26 13:02:24 +0530

    implement Account API with resource and controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('accounts', AccountController::class)->only(['index']);
+});
