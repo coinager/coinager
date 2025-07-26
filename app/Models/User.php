c4958426c493f2a36006d4e0abbdd3a33bc03678
Chinmay Purav <chinpurav@gmail.com>
2025-07-26 09:03:28 +0530

    feat: set up API routes and integrate Laravel Sanctum for authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements FilamentUser
 {
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
